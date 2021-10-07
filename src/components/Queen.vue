57730aad1fd87b169d105d4eb46f5d11a6c1f19e
Chuy <chuyalexgt@gmail.com>
2021-10-07 17:11:30 -0500

    optimizacion del movimiento de la reina (rango anadido)

--- src/components/Queen.vue
+++ src/components/Queen.vue
@@ -38,123 +38,107 @@ export default {
     this.$bus.$on("positionsToMoveQueen", (data) => {
       //movimiento de reina
       if ((data.start[0] == this.x) & (data.start[1] == this.y)) {
+        //Si es la ficha que seleccionaste y la casilla a la que se quiere mover esta dentro del rango, ejecuta el movimiento
+        if (this.chessboardMatriz[data.end[1]][data.end[0]].inRange)
+          this.$bus.$emit("executeMovement", data);
+        else this.$bus.$emit("invalidMovement");
+      }
+    });
+    this.$bus.$on("rangeToMoveQueen", (position) => {
+      if ((position[0] == this.x) & (position[1] == this.y)) {
         //Si es la ficha que seleccionaste...
-        this.queenMovement(data);
+        this.linearMovementRange(position);
+        this.diagonalMovementRange(position);
       }
     });
   },
   methods: {
-    queenMovement(data) {
-      let diagonalValidation =
-        Math.abs(data.start[0] - data.end[0]) === Math.abs(data.start[1] - data.end[1]);
-      let linearValidation =
-        ((data.start[0] != data.end[0]) & (data.start[1] == data.end[1])) |
-        ((data.start[0] == data.end[0]) & (data.start[1] != data.end[1]));
-      let dontKillFriends = data.pieceData.color != data.positionData.color;
-      if (diagonalValidation) {
-        this.diagonalDontFlyValidation = this.diagonalDontFly(data); ///validacion para que no salte otras piezas
-        if (diagonalValidation & dontKillFriends & this.diagonalDontFlyValidation) {
-          this.diagonalDontFlyValidation = false;
-          this.$bus.$emit("executeMovement", data);
-        } else {
-          this.$bus.$emit("invalidMovement");
-        }
-        return
-      }
-      if (linearValidation) {
-        this.linearDontFlyValidation = this.linearDontFly(data); ///validacion para que no salte otras piezas
-        if (linearValidation & dontKillFriends & this.linearDontFlyValidation) {
-          this.linearDontFlyValidation = false;
-          this.$bus.$emit("executeMovement", data);
-        } else {
-          this.$bus.$emit("invalidMovement");
-        }
-      } else {
-        this.$bus.$emit("invalidMovement");
-      }
+    diagonalMovementRange(position) {
+      let xyMax, _xyMax, _x_yMax, x_yMax; //el _ representa signo -, cada variable es la componente diagonal de cada cuadrante
+      Math.abs(position[0] - 0) <= Math.abs(position[1] - 7) //se calcula el valor maximo de la componente diagonal de cada cuadrante
+        ? (xyMax = Math.abs(position[0] - 0))
+        : (xyMax = Math.abs(position[1] - 7));
+      Math.abs(position[0] - 0) <= Math.abs(position[1] - 0)
+        ? (_xyMax = Math.abs(position[0] - 0))
+        : (_xyMax = Math.abs(position[1] - 0));
+      Math.abs(position[0] - 7) <= Math.abs(position[1] - 0)
+        ? (_x_yMax = Math.abs(position[0] - 7))
+        : (_x_yMax = Math.abs(position[1] - 0));
+      Math.abs(position[0] - 7) <= Math.abs(position[1] - 7)
+        ? (x_yMax = Math.abs(position[0] - 7))
+        : (x_yMax = Math.abs(position[1] - 7));
+      // let dontKillFriends = data.pieceData.color != data.positionData.color;
+      this.diagonalDontFly(position, xyMax, _xyMax, _x_yMax, x_yMax); ///funcion para validar que no salten otras piezas y colorear las casillas a las que se puede mover
     },
-
-    diagonalDontFly(data) {
-      let start = data.start;
-      let end = data.end;
-      let colission = false;
-      let stepLenght = Math.abs(start[0] - end[0]);
-      if ((start[0] <= end[0]) & (start[1] <= end[1])) {
-        //mov. en cuadrante 1
-        for (let i = 1; i < stepLenght; i++) {
-          if (this.chessboardMatriz[start[1] + i][start[0] + i].content != "") {
-            colission = true;
-          }
-        }
+    diagonalDontFly(position, xyMax, _xyMax, _x_yMax, x_yMax) {
+      let start = position;
+      let cellsInRange = [];
+      //mov. en cuadrante 4
+      for (let i = 1; i <= x_yMax; i++) {
+        if (this.chessboardMatriz[start[1] + i][start[0] + i].color == this.teamColor)
+          break;
+        cellsInRange.push([start[1] + i, start[0] + i]);
+        if (this.chessboardMatriz[start[1] + i][start[0] + i].content != "") break;
       }
-      if ((start[0] >= end[0]) & (start[1] <= end[1])) {
-        //mov. en cuadrante 2
-        for (let i = 1; i < stepLenght; i++) {
-          if (this.chessboardMatriz[start[1] + i][start[0] - i].content != "") {
-            colission = true;
-          }
-        }
+      //mov. en cuadrante 1
+      for (let i = 1; i <= xyMax; i++) {
+        if (this.chessboardMatriz[start[1] + i][start[0] - i].color == this.teamColor)
+          break;
+        cellsInRange.push([start[1] + i, start[0] - i]);
+        if (this.chessboardMatriz[start[1] + i][start[0] - i].content != "") break;
       }
-      if ((start[0] >= end[0]) & (start[1] >= end[1])) {
-        //mov. en cuadrante 3
-        for (let i = 1; i < stepLenght; i++) {
-          if (this.chessboardMatriz[start[1] - i][start[0] - i].content != "") {
-            colission = true;
-          }
-        }
+      //mov. en cuadrante 2
+      for (let i = 1; i <= _xyMax; i++) {
+        if (this.chessboardMatriz[start[1] - i][start[0] - i].color == this.teamColor)
+          break;
+        cellsInRange.push([start[1] - i, start[0] - i]);
+        if (this.chessboardMatriz[start[1] - i][start[0] - i].content != "") break;
       }
-      if ((start[0] <= end[0]) & (start[1] >= end[1])) {
-        //mov. en cuadrante 4
-        for (let i = 1; i < stepLenght; i++) {
-          if (this.chessboardMatriz[start[1] - i][start[0] + i].content != "") {
-            colission = true;
-          }
-        }
+      //mov. en cuadrante 3
+      for (let i = 1; i <= _x_yMax; i++) {
+        if (this.chessboardMatriz[start[1] - i][start[0] + i].color == this.teamColor)
+          break;
+        cellsInRange.push([start[1] - i, start[0] + i]);
+        if (this.chessboardMatriz[start[1] - i][start[0] + i].content != "") break;
       }
-      if (!colission) return true;
-      else return false;
+      this.$bus.$emit("renderCellsInRange", cellsInRange);
     },
-    linearDontFly(data) {
-      let start = data.start;
-      let end = data.end;
-      let colission = false;
-      let xdiferential = Math.abs(start[0] - end[0]);
-      let ydiferential = Math.abs(start[1] - end[1]);
-
-      if ((start[0] <= end[0]) & (start[1] == end[1])) {
-        //mov. en x
-        for (let i = 1; i < xdiferential; i++) {
-          if (this.chessboardMatriz[start[1]][start[0] + i].content != "") {
-            colission = true;
-          }
-        }
+    linearMovementRange(position) {
+      let xMax, yMax, _xMax, _yMax; //cada variable es la componente vertical/horizontal del plano
+      xMax = Math.abs(position[1] - 7); //➡
+      yMax = Math.abs(position[0] - 0); //⬆
+      _xMax = Math.abs(position[1] - 0); //⬅
+      _yMax = Math.abs(position[0] - 7); //⬇
+      this.linearDontFly(position, xMax, yMax, _xMax, _yMax); ///validacion para que no salte otras piezas colorear las casillas a las que se puede mover
+    },
+    linearDontFly(position, xMax, yMax, _xMax, _yMax) {
+      let start = position;
+      let cellsInRange = [];
+      //mov. en x
+      for (let i = 1; i <= xMax; i++) {
+        if (this.chessboardMatriz[start[1] + i][start[0]].color == this.teamColor) break;
+        cellsInRange.push([start[1] + i, start[0]]);
+        if (this.chessboardMatriz[start[1] + i][start[0]].content != "") break;
       }
-      if ((start[0] == end[0]) & (start[1] <= end[1])) {
-        //mov. en y
-        for (let i = 1; i < ydiferential; i++) {
-          if (this.chessboardMatriz[start[1] + i][start[0]].content != "") {
-            colission = true;
-          }
-        }
+      //mov. en y
+      for (let i = 1; i <= yMax; i++) {
+        if (this.chessboardMatriz[start[1]][start[0] - i].color == this.teamColor) break;
+        cellsInRange.push([start[1], start[0] - i]);
+        if (this.chessboardMatriz[start[1]][start[0] - i].content != "") break;
       }
-      if ((start[0] >= end[0]) & (start[1] == end[1])) {
-        //mov. en -x
-        for (let i = 1; i < xdiferential; i++) {
-          if (this.chessboardMatriz[start[1]][start[0] - i].content != "") {
-            colission = true;
-          }
-        }
+      //mov. en -x
+      for (let i = 1; i <= _xMax; i++) {
+        if (this.chessboardMatriz[start[1] - i][start[0]].color == this.teamColor) break;
+        cellsInRange.push([start[1] - i, start[0]]);
+        if (this.chessboardMatriz[start[1] - i][start[0]].content != "") break;
       }
-      if ((start[0] == end[0]) & (start[1] >= end[1])) {
-        //mov. en -y
-        for (let i = 1; i < ydiferential; i++) {
-          if (this.chessboardMatriz[start[1] - i][start[0]].content != "") {
-            colission = true;
-          }
-        }
+      //mov. en -y
+      for (let i = 1; i <= _yMax; i++) {
+        if (this.chessboardMatriz[start[1]][start[0] + i].color == this.teamColor) break;
+        cellsInRange.push([start[1], start[0] + i]);
+        if (this.chessboardMatriz[start[1]][start[0] + i].content != "") break;
       }
-      if (!colission) return true;
-      else return false;
+      this.$bus.$emit("renderCellsInRange", cellsInRange);
     },
   },
   computed: {
